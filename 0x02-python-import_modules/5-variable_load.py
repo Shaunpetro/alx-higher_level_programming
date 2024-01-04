7e68f817743941c0be18ff78979379079c7fea6b
shaunpetro <shaun.p.mckoy@gmail.com>
2024-01-04 18:38:18 +0000

    3-infinite_add.py fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,13 +2,8 @@
 from variable_load_5 import a
 
 
-
-
 def find_var():
     print(a)
-
-
-
 
     if __name__ == "__main__":
         find_var()
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
+
 def add(a, b):
     """My addition function
 
     Args:
-    a: first integer
-    b: second integer
+        a: first integer
+        b: second integer
 
     Returns:
-    The return value. a + b
+        The return value. a + b
     """
     return (a + b)
 
@@ -16,36 +18,36 @@ def sub(a, b):
     """My subtraction function
 
     Args:
-    a: first integer
-    b: second integer
+        a: first integer
+        b: second integer
 
     Returns:
-    The return value. a - b
+        The return value. a - b
     """
     return (a - b)
 
 
-    def mul(a, b):
+def mul(a, b):
     """My multiplication function
 
     Args:
-    a: first integer
-    b: second integer
+        a: first integer
+        b: second integer
 
     Returns:
-    The return value. a * b
+        The return value. a * b
     """
     return (a * b)
 
 
-    def div(a, b):
+def div(a, b):
     """My division function
 
     Args:
-    a: first integer
-    b: second integer
+        a: first integer
+        b: second integer
 
     Returns:
-    The return value. a / b
+        The return value. a / b
     """
     return int(a / b)
