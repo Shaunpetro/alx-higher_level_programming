f464567323faeff3ec790329b73983cef6eb201b
shaunpetro <shaun.p.mckoy@gmail.com>
2024-01-22 15:13:57 +0000

    task correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
 
@@ -18,4 +19,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (ret
+    return (ret)
