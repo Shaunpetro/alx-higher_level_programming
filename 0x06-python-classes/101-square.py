906126d75345cceaccc5c1f1db68820ea1243806
shaunpetro <shaun.p.mckoy@gmail.com>
2024-01-23 15:57:35 +0000

    optional task files

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -41,31 +41,31 @@ class Square:
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-            self.__position = value
+        self.__position = value
 
-            def area(self):
-                """Return the current area of the square."""
-                return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-            def my_print(self):
-                """Print the square with the # character."""
-                if self.__size == 0:
-                    print("")
-                    return
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-                [print("") for i in range(0, self.__position[1])]
-                for i in range(0, self.__size):
-                    [print(" ", end="") for j in range(0, self.__position[0])]
-                    [print("#", end="") for k in range(0, self.__size)]
-                    print("")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
-            def __str__(self):
-                """Define the print() representation of a Square."""
-                if self.__size != 0:
-                    [print("") for i in range(0, self.__position[1])]
-                for i in range(0, self.__size):
-                    [print(" ", end="") for j in range(0, self.__position[0])]
-                    [print("#", end="") for k in range(0, self.__size)]
-                    if i != self.__size - 1:
-                        print("")
-                 return ("")
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
